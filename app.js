65d064bd2fa7a5a8d5824e7dbee294098025c144
Gabicle <gskurah@yahoo.co.uk>
2019-09-17 20:54:20 +0100

    Updated port, added profile and gitignore

--- app.js
+++ app.js
@@ -76,6 +76,6 @@ app.post("/login", function(req,res){
   });
 });
 
-app.listen(3000, function(req, res){
-  console.log("Server Started on Port 3000");
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Server is running on port 3000");
 });
