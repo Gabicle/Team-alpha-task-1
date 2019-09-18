36b53da80774cba9bf682ed69692d60d93911c2e
Gabicle <gskurah@yahoo.co.uk>
2019-09-18 15:46:13 +0100

    md5 Authentication

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 // const encrypt = require('mongoose-encryption');
 
 const app = express();
@@ -26,8 +26,8 @@ const userSchema = new mongoose.Schema(
     password: String
   }
 ) ;
-// Low-quality encryption of passwords
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
+// // Low-quality encryption of passwords
+// userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ['password'] });
 
 // Using userSchema to create a mongoose model with collection named User
 const User = new mongoose.model("User", userSchema);
@@ -45,7 +45,7 @@ app.post("/register", function(req, res){
   const newUser = new User({
     name: req.body.fname,
     email: req.body.username,
-    password: req.body.password
+    password: md5(req.body.password)
   });
   // Saving the new user
   newUser.save(function(error){
@@ -61,7 +61,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req,res){
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, function(error, foundUser){
     if(error){
